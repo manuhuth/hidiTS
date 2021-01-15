880dfa3b2eeb9487750c186ca339e7f84e0047e7
McKers <64460049+McKers@users.noreply.github.com>
2021-01-15 11:09:53 +0100

    Update functions.
    Now including restrictions/forms for matrices. Option to perform kalman filtering and likelihood estimation repeatedly. f_estimate still buggy -> avoid.

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,33 +1,257 @@
+#'auxiliary function in gamma_matrix that creates diag small gamma matrices
+#'@param chuncks list of equially sized data clusters
+#'@return list of gamma matrices
+create_gammas <- function(chunks){
+  
+  gammas <- vector(mode = "list", length = length(chunks))
+  for (i in 1:length(chunks)){
+    
+    gammas[[i]] <- diag(x=chunks[[i]],q,q)
+    #print(gammas[[i]])
+    
+  }
+  
+  return(gammas)
+}
+
+#' auxiliary function in gamma_matrix that creates zero matrices 
+#' @param p number of lags for factors
+#' @param q number of factors per period
+#' @param k order of gamma lag polynomial/number of gamma matrices
+#' @return zeros matrices 
+create_zeros <- function(p,q,k){
+  
+  number_zeros <- (p+1)-k
+  if(number_zeros <= 0){
+    zeros <- vector(mode = "list", length = 0)
+  } else {
+    zeros <- vector(mode = "list", length = number_zeros)
+    for (i in 1:number_zeros){
+      
+      zeros[[i]] <- diag(x=0,q,q)
+      #print(gammas[[i]])
+    }
+  }
+  
+  return(zeros)
+}
+
+#' creates Gamma matrix as  described in paper
+#' @param p number of lags for factors
+#' @param q number of factors per period
+#' @param k order of gamma lag polynomial 
+#' @param data vector of data points to supply
+#' @param restricted if TRUE restricts small gammas to lag structure
+#' @return Gamma matrix 
+gamma_matrix <- function(p,q,k,data,restricted=TRUE){
+  
+  if(isTRUE(restricted)){
+    
+    gammas <- vector(mode = "list", length = k)
+    
+    for(i in 1:k){
+      gammas[[i]] <- diag(x=data^i,q)
+    }
+    
+    
+  } else {
+    chunks <- split(data, ceiling(seq_along(data)/q))
+    
+    gammas <- create_gammas(chunks = chunks)  
+    
+    
+  }
+  
+  zeros <- create_zeros(p=p,q=q,k=k)
+  
+  matrices <- c(gammas[1:(k)],zeros)
+  
+  upper <- do.call(cbind, matrices)
+  
+  m <- length(matrices)
+  n <- ncol(matrices[[1]])
+  lower_diag <- diag((m - 1) * q) # create lower block matrix
+  
+  zero_right <- matrix(rep(0, len = (m - 1) * q^2), ncol = n, nrow = (m - 1) * n)
+  lower <- cbind(lower_diag, zero_right)
+  
+  output <- rbind(upper, lower)
+  
+  return(output)
+  
+}
+
+#' creates Lambda matrix as  described in paper
+#' @param n number of observable x/number of time series
+#' @param p number of lags for factors
+#' @param q number of factors per period
+#' @param data vector of data points to supply
+#' @param restricted if TRUE restricts Lambda to structure by Bai,Ng (2013)  
+#' @return Gamma matrix 
+lambda_matrix <-function(data,n,p,q,restricted=TRUE){
+  
+  if(isTRUE(restricted)){
+    
+    upper <- matrix(data = rep(0),nrow=(p+1)*q,ncol=(p+1)*q)
+    nu <- 0.5*(1+(p+1)*q)*((p+1)*q)
+    upper[lower.tri(upper,diag = TRUE)] <- data[1: nu]
+    
+    #lower matrix
+    nl <- (((n-(p+1)*q)*(p+1)*q))
+    lower <- matrix(data[nu+1:nl],nrow=n-(p+1)*q,ncol=(p+1)*q)
+    lambda <- rbind(upper,lower)
+    
+    return(lambda)
+    
+    
+  }else{
+    
+    lambda <- matrix(data=data, nrow = n, ncol = (p+1)*q)
+    
+    return(lambda)
+    
+  }
+}
+
+#' creates symmetric matrix of dimension n x n
+#' @param data vector of data points to supply
+#' @param n dimension
+#' @return symmetric matrix 
+sigma_u_matrix <- function(data,n,diag_res=FALSE){
+  
+  if(isTRUE(diag_res)){
+    sigma_u <-diag(x=data,n,n)
+    return(sigma_u)
+  } else  {
+    
+    sigma_u <-diag(x=data[1:n],n,n)
+    sigma_u[lower.tri(sigma_u,diag = FALSE)] <- data[n+1: (length(data)-n)]
+    sigma_u[upper.tri(sigma_u,diag = FALSE)] <- t(sigma_u)[upper.tri(t(sigma_u),diag = FALSE)]
+    
+    #sigma_u[upper.tri(sigma_u,diag = FALSE)] <- data_par[n+1: length(data)]
+    return(sigma_u)
+  }
+  
+}
+
+#' creates sigma_e matrix as described in paper
+#' @param p number of lags for factors 
+#' @param q number of factors per period
+#' @param data vector of data points to supply
+#' @return symmetric matrix 
+sigma_e_matrix <- function(p,q,data){
+  #create 0 matrix 
+  sigma_e <- matrix(data=0, nrow = (p+1)*q, ncol = (p+1)*q)
+  #create sigma_eta
+  diag_m <- diag(data,q,q)
+  #place sigma_eta
+  sigma_e[1:q,1:q] <- diag_m
+  
+  return(sigma_e)
+  
+}
+
+
+
+
 # maps initial values from column vector into matrix form for optim
-theta_wrapper <- function(theta,n,p,q){
+#' wraps data points in matrices in theta
+#' @param data vector of data points to supply
+#' @param n number of observable x/number of time series
+#' @param p number of lags for factors 
+#' @param q number of factors per period
+#' @param k order of gamma lag polynomial 
+#' @param gamma_res if TRUE restricts small gammas to lag structure
+#' @param lambda_res if TRUE restricts Lambda to structure by Bai,Ng (2013)  
+#' @return number of paramaters over all matrices in theta
+matrix_form <- function(data,n,p,q,k,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=FALSE){
   
+  if(isTRUE(gamma_res)){
+    last_gamma <- q
+    gamma <- gamma_matrix(p=p,q=q,k=k,data = data[1:q],restricted=TRUE)
+    
+  }  else {
+    last_gamma <- k*q
+    gamma <- gamma_matrix(p=p,q=q,k=k,data = data[1:(k*q)],restricted=FALSE)
+    
+  }
   
-  last_gamma <- (p+1)*q*(p+1)*q
-  gamma <- matrix(data=theta[1:last_gamma],nrow=(p+1)*q,ncol = (p+1)*q)
+  if(isTRUE(lambda_res)){
+    last_lambda <- last_gamma + 0.5*(1+(p+1)*q)*((p+1)*q) + ((n-(p+1)*q)*(p+1)*q)
+    lambda <- lambda_matrix(data=data[last_gamma+1:last_lambda],n=n,p=p,q=q,restricted = TRUE)
+    
+  }  else {
+    last_lambda <- last_gamma + n*(p+1)*q
+    lambda <- lambda_matrix(data=data[last_gamma+1:last_lambda],n=n,p=p,q=q,restricted = FALSE)
+    
+  }
   
-  
-  
-  last_lambda <- last_gamma + (n*(p+1)*q)
-  lambda <- matrix(data=theta[((last_gamma)+1):(last_lambda)],nrow=n,ncol = (p+1)*q)
-  
-  
-  #sigma_u
-  last_sigma_u <- last_lambda +(n*n)
-  sigma_u <- matrix(data=theta[((last_lambda)+1):(last_sigma_u) ],nrow=n,ncol = n)
-  
+  if(sigma_u_diag==FALSE){
+    #sigma_u
+    last_sigma_u <- last_lambda + 0.5*(1+n)*n
+    sigma_u <- sigma_u_matrix(data=data[((last_lambda)+1):(last_sigma_u) ],n=n,diag_res=FALSE)
+    
+  } else {
+    last_sigma_u <- last_lambda + n
+    sigma_u <- sigma_u_matrix(data=data[((last_lambda)+1):(last_sigma_u) ],n=n,diag_res=TRUE)  
+    
+  }
   #sigma_e
-  last_sigma_e <- last_sigma_u + ((p+1)*q*(p+1)*q)
-  sigma_e <- matrix(data=theta[((last_sigma_u)+1):(last_sigma_e) ],nrow=(p+1)*q,ncol = (p+1)*q)
+  
+  sigma_e <- sigma_e_matrix(data=data[(last_sigma_u+1):(length(data))],p=p,q=q)
   
   output <- list("gamma" = gamma, "lambda" = lambda, "sigma_u" = sigma_u, "sigma_e" = sigma_e)
   
   return(output) 
+  
 }
 
 
+
 # gives number of rows for theta vector to create initial values for optimization
-theta_rows <- function(n,p,q){
-  rows=2*((p+1)*q)^2+n*(p+1)*q+n^2
+number_of_param <- function(n,p,q,k,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=FALSE){
+  
+  F_dim <- (p+1)*q
+  
+  #lambda paramaters
+  
+  #restricted lambda
+  if(isTRUE(lambda_res)) {
+    p_lambda <- 0.5*(1+F_dim)*F_dim + (n-F_dim)*F_dim
+  } else {
+    #unrestricted lambda
+    p_lambda <- n*F_dim
+  }
+  #gamma paramaters
+  
+  #restricted gamma
+  if(isTRUE(gamma_res)) {
+    
+    p_gamma <- q
+    
+  } else {
+    
+    #unrestricted gamma
+    p_gamma <- q*k
+  }
+  
+  #sigma_u paramaters
+  if(sigma_u_diag==TRUE) {
+    
+    p_sigma_u <- n
+    
+  } else {
+    
+    p_sigma_u <- 0.5*(1+n)*n
+    
+  }
+  
+  
+  #sigma_e paramters
+  p_sigma_e <- q
+  
+  
+  rows =  p_lambda + p_gamma + p_sigma_u + p_sigma_e
   
   return(rows)
   
@@ -53,85 +277,6 @@ f_init <- function(p,q){
 
 
 
-post_prediction_sigma_f <- function(sigma_f_prior,lambda,sigma_x_prior){
-  
-  sigma_f_post <- sigma_f_prior - sigma_f_prior %*% t(lambda) %*% solve(sigma_x_prior) %*% lambda %*% sigma_f_prior 
-  
-  return(sigma_f_post)
-  
-}
-
-
-post_prediction_f <-  function(lambda,f_prior,sigma_f_prior,sigma_x_prior,data,x_prior){
-  
-  f_post  <- f_prior + sigma_f_prior %*% t(lambda) %*%solve(sigma_x_prior) %*%  (data - x_prior)
-  
-  return(f_post)
-}
-
-
-x_prediction <- function(lambda,f_prior){
-  x_prior <- lambda %*% f_prior
-  
-  return(x_prior)
-}
-
-
-sigma_x_prediction <- function(lambda,sigma_u,sigma_f_prior){
-  sigma_x_prior <- lambda %*% sigma_f_prior %*% t(lambda) + sigma_u 
-  return(sigma_x_prior)
-}
-
-
-f_prediciton <- function(gamma,f_post){
-  f_prior <- gamma %*% f_post
-  
-  return(f_prior)
-}
-
-
-sigma_f_prediction <- function(gamma,sigma_f_post,sigma_e){
-  sigma_f_prior <- gamma %*% sigma_f_post %*% t(gamma)  + sigma_e
-  
-  return(sigma_f_prior)
-}
-
-
-kalman_wrapper_inital_period <- function(lambda,sigma_u,f_inital,sig_f_inital,data){
-  
-  #prior observation prediction 
-  x_pred <-  x_prediction(lambda = lambda, f_prior = f_inital)
-  sig_x_pred <- sigma_x_prediction(lambda=lambda, sigma_u = sigma_u, sigma_f_prior = sig_f_inital)
-  # post facor predictiom
-  f_post <- post_prediction_f(lambda=lambda,f_prior=f_inital,sigma_f_prior=sig_f_inital,x_prior=x_pred,sigma_x_prior=sig_x_pred,data=data)
-  sigma_f_post <- post_prediction_sigma_f(sigma_f_prior=sig_f_inital,sigma_x_prior=sig_x_pred,lambda=lambda)
-  
-  output <- list("f_post"=f_post,"sigma_f_post"=sigma_f_post)
-  
-  return(output)
-  
-}
-
-
-kalman_wrapper <- function(gamma,lambda,sigma_u,sigma_e,f_post_last,sigma_f_post_last,data){
-  
-  #prior factor prediction
-  f_pred <- f_prediciton(gamma=gamma,f_post = f_post_last)
-  sig_f_pred <- sigma_f_prediction(gamma=gamma,sigma_f_post = sigma_f_post_last,sigma_e = sigma_e)
-  #prior observation prediction 
-  x_pred <-  x_prediction(lambda = lambda, f_prior = f_pred)
-  sig_x_pred <- sigma_x_prediction(lambda=lambda, sigma_u = sigma_u, sigma_f_prior = sig_f_pred)
-  # post facor predictiom
-  f_post <- post_prediction_f(lambda=lambda,f_prior=f_pred,sigma_f_prior=sig_f_pred,x_prior=x_pred,sigma_x_prior=sig_x_pred,data = data)
-  sigma_f_post <- post_prediction_sigma_f(sigma_f_prior=sig_f_pred,sigma_x_prior=sig_x_pred,lambda=lambda)
-  
-  output <- list("f_post"=f_post,"sigma_f_post"=sigma_f_post)
-  
-  
-  return(output)
-}
-
-
 # calcualtes the log density given paramters using the initial guess from kalman filter
 likelihood_init <- function(gamma,lambda,sigma_u,sigma_e,data,p,q){
   
@@ -149,11 +294,22 @@ likelihood <- function(gamma,lambda,sigma_u,sigma_e,f_post,sigma_f_post,data){
 }
 
 
-
-
-wrapper <- function(theta,data,n,p,q,t){
+#' calculates the likelihood as  specified in paper
+#' @param data_param vector of data points to supply
+#' @param data_x matrix data of the observable time series
+#' @param n number of observable x/number of time series
+#' @param p number of lags for factors 
+#' @param q number of factors per period
+#' @param k order of gamma lag polynomial 
+#' @param t time dimension of observable x/time series
+#' @param gamma_res if TRUE restricts small gammas to lag structure
+#' @param lambda_res if TRUE restricts Lambda to structure by Bai,Ng (2013)  
+#' @param post_F list of post_F estimations by Kalman Filter
+#' @param post_P list of post_P/sigma_F estimations by Kalman Filter
+#' @return sum of likelihood values across time
+likelihood_wrapper <- function(data_param,data_x,n,p,q,k,t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=FALSE,post_F,post_P){
   
-  matrices <- theta_wrapper(theta=theta,n=n,p=p,q=q)
+  matrices <- matrix_form(data=data_param,n=n,p=p,q=q,k=k,gamma_res=gamma_res,lambda_res=lambda_res,sigma_u_diag=sigma_u_diag)
   
   
   gamma <- matrices$gamma
@@ -161,33 +317,100 @@ wrapper <- function(theta,data,n,p,q,t){
   sigma_u <- matrices$sigma_u
   sigma_e <- matrices$sigma_e
   
-  # initliaze list 
-  post_f= vector(mode = "list", length = 0)
-  post_sigma_f= vector(mode = "list", length = 0)
   likelihood_list <- vector(mode = "numeric", length = 0)
-  
-  
   #first period
-  likelihood_list[[1]] <- likelihood_init(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,data = data[,1],p=p,q=q) 
-  
-  # save first values
-  post_f[[1]] <- kalman_wrapper_inital_period(lambda=lambda,sigma_u=sigma_u,f_inital=f_init(q=q,p=p),sig_f_inital=sigma_f_init(p=p,q=q,gamma=gamma,sigma_e=sigma_e),data = data[,1])$f_post
-  post_sigma_f[[1]] <- kalman_wrapper_inital_period(lambda=lambda,sigma_u=sigma_u,f_inital=f_init(q=q,p=p),sig_f_inital=sigma_f_init(p=p,q=q,gamma=gamma,sigma_e=sigma_e),data = data[,1])$sigma_f_post
-  
+  likelihood_list[[1]] <- likelihood_init(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,data = data_x[[2]][,1],p=p,q=q) 
   
   #loop for second period and so on and so on
   
   for(i in 2:t){
     
-    likelihood_list[[i]] <- likelihood(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,f_post=post_f[[i-1]],sigma_f_post=post_sigma_f[[i-1]],data = data[,i])
-    #save second values
-    post_f[[i]] <- kalman_wrapper(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,f_post_last=post_f[[i-1]],sigma_f_post_last=post_sigma_f[[i-1]],  data = data[,i])$f_post
-    post_sigma_f[[i]] <- kalman_wrapper(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,f_post_last=post_f[[i-1]],sigma_f_post_last=post_sigma_f[[i-1]],  data = data[,i])$sigma_f_post
+    likelihood_list[[i]] <- likelihood(gamma=gamma,lambda=lambda,sigma_u=sigma_u,sigma_e=sigma_e,f_post=post_F[[i-1]],sigma_f_post=post_P[[i-1]],data = data_x[[2]][,i])
+    
   }
   
   output <- sum(likelihood_list)
   
-  return(-output)
+  return(output)
   
 }
 
+optim_wrapper <- function(data_param,optim_func,data_x,n,p,q,k,t,post_F,post_P,method = "Nelder-Mead"){
+  
+  
+  rslt=optim(par=data_param, fn=optim_func,data_x=data_x,n=n,p=p,q=q,k=k,t=t,post_F=post_F,post_P=post_P,control=list(fnscale=-1),method = method)
+  #rslt$par
+  
+  params <- rslt$par 
+  
+  matrices <- matrix_form(rslt$par,n=n,p=p,q=q,k=k)
+  
+  gamma <- list(matrices$gamma)
+  
+  lambda <- list(matrices$lambda)
+  
+  sigma_e <- list(matrices$sigma_e)
+  
+  sigma_u <- list(matrices$sigma_u)
+  
+  Kalman_rslt <- Kalman(q=q,p=p,T=t,n=n, lambda=lambda, gamma=gamma, Sigma_e=sigma_e, Sigma_u=sigma_u ,start_ar=0,X=data_test)
+  
+  Fsmooth_upd <- Kalman_rslt$Fsmooth
+  Psmooth_upd <- Kalman_rslt$Psmooth
+  
+  #post_F_list[[(j+1)]] <- Fsmooth_upd
+  #post_P_list[[(j+1)]] <- Psmooth_upd
+  
+  #post_F_list <<- post_F_list
+  
+  #post_P_list <<- post_P_list  
+  
+  output <- list("params" = params, "post_F" = Fsmooth_upd, "post_P" =Psmooth_upd)
+  return(output)
+  
+  
+  
+}
+
+estimate_f <- function(data_param,data_x,n,p,q,k,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=FALSE,it=4,method = "Nelder-Mead"){
+  
+  
+  matrices <- matrix_form(data=data_param,n=n,p=p,q=q,k=k,gamma_res=gamma_res,lambda_res=lambda_res,sigma_u_diag=sigma_u_diag)
+  
+  lambda <- list(matrices$lambda)
+  
+  gamma <- list(matrices$gamma)
+  
+  sigma_e <- list(matrices$sigma_e)
+  
+  sigma_u <- list(matrices$sigma_u)  
+  
+  Kalman_first <- Kalman(q=q,p=p,T=t,n=n, lambda=lambda, gamma=gamma, Sigma_e=sigma_e, Sigma_u=sigma_u ,start_ar=0,X=data_x)
+  
+  fsmooth1 <- Kalman_first$Fsmooth
+  psmooth1 <- Kalman_first$Psmooth
+  
+  list_param= vector(mode = "list", length = 0)
+  list_f= vector(mode = "list", length = 0)
+  list_sigma_f= vector(mode = "list", length = 0)
+  
+  list_param[[1]] <- data_param_init
+  list_f[[1]] <- fsmooth1
+  list_sigma_f[[1]] <- psmooth1
+  
+  # perform loop with likelihood estimation  for parameters and kalman filter
+  
+  counter <- 1
+  
+  while (counter <= it){
+    
+    rslt_while_counter <- optim_wrapper(data_param=list_param[[(length(list_param))]],optim_func=likelihood_wrapper,data_x=data_x,n=n,p=p,q=q,k=k,t=t,post_F=list_f[[(length(list_f))]],post_P=list_sigma_f[[(length(list_sigma_f))]],method = method)
+    print(rslt_while_counter$params)
+    list_param <- append(list_param,list(rslt_while_counter$params))
+    list_f[counter+1] <- rslt_while_counter$post_F
+    list_sigma_f <- append(list_sigma_f, list(rslt_while_counter$post_P))
+    
+    counter <- counter + 1
+    
+  }
+  
