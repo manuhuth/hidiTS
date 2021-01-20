97b30f843bcf7a22f6a7d6b8259ee187c73a6d91
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-01-20 17:35:00 +0100

    Add parallel playground.

--- tests/irgendwie_witzig.R
+++ tests/irgendwie_witzig.R
@@ -1,17 +1,24 @@
 #devtools::install(dependencies = TRUE)
 
 library(hidiTS)
+#library("optimParallel")
 rm(list = ls())
 
 
-
 #parallel computing
+cl <- makeCluster(detectCores()-1)
 
-for (index in 1:10) {
+setDefaultCluster(cl = cl)
+
+
+set.seed(42)
+start <- Sys.time()
+for (index in 1:50) {
+
 n=10
-p=2
-q=3
-t=30
+p=0
+q=2
+t=20
 k=1
 
   data_test <- sim_data(p = n, T = t, dim_F= q, lags_F=k, lags_X=p, ar_F=1, ar_Y=1, A = NULL, low_X = -3^0.5, up_X = 3^0.5,
@@ -20,35 +27,24 @@ k=1
                         burn_in = 20, data_only = FALSE,
                         only_stationary = TRUE, epsilon = 0.0002, max_it_station = 500, adjust_diag = FALSE,
                         geometric_F =TRUE, diag_F = TRUE, geometric_X =FALSE, geometric_Y =FALSE)
-  
+  clusterExport(cl, list('n', 'p', 'q', 't', 'k'))
   est <- estimate_f(data_x=data_test,n=n,p=p,q=q,k=k,t=t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=TRUE,it=1,
-             method = "L-BFGS-B", parallel = FALSE, max_it = 5)
+             method = "L-BFGS-B", parallel = FALSE, max_it = 30, trace=0)
+  #est <- estimate_f(data_x=data_test,n=n,p=p,q=q,k=k,t=t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=TRUE,it=1,
+  #                  method = "L-BFGS-B", parallel = TRUE, max_it = 30, trace=0, forward = TRUE, loginfo=FALSE)
   print(index)
 }
+end <- Sys.time()
+end - start 
 
 
+normal <- end - start
+parallel_normal <- end - start
+parallel_forward <- end - start
 
-its <- c(50)
-est <- list()
-for (index in 1:length(its)) {
-  est[[index]] <- estimate_f(data_x=data_test,n=n,p=p,q=q,k=k,t=t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=TRUE,it=its[index],
-                     method = "L-BFGS-B", parallel = FALSE, max_it = 3)
-  print(its[index])
+f <- convert_factors_ML(est$lambda[[1]], est$f_final, q)$F
+f_d <- convert_factors_dgp(data_test)$F
+plot(f[1,], type = 'l')
+lines(-f_d[1,], type='l', col='blue')
 
-}
 
-matrix <- c()
-#convert_ML
-for (index in 1:length(its)) {
-  val <- convert_factors_ML(Lambda=est[[index]]$lambda[[1]], factors = est[[index]][['f_final']], q=q)$F[1,] 
-  if (val[1] > 0) {
-    val <- -val
-  }
-  matrix <- cbind(matrix, val)
-}
-
-#convert data
-dgp <- convert_factors_dgp(data_test)
-
-matplot(matrix, type = 'l')
-lines(dgp$F[1,], type='l', col='red')
