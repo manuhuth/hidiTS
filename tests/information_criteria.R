c88ec6d04b92a0d637c3985629904095d18211ca
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-01-21 13:32:33 +0100

    Add clean version of information criteria

--- tests/information_criteria.R
+++ tests/information_criteria.R
@@ -1,11 +1,18 @@
+devtools::install(dependencies = TRUE)
 library(hidiTS)
-devtools::install_github("manuhuth/hidiTS")
+library(optimParallel)
+
+
 rm(list = ls())
 set.seed(123)
-n=10
+
+cl <- makeCluster(detectCores()-1)
+setDefaultCluster(cl = cl)
+
+n=50
 p=1
 q=2
-t=15
+t=40
 k=1
 
 data_test <- sim_data(p = n, T = t, dim_F= q, lags_F=k, lags_X=p, ar_F=1, ar_Y=1, A = NULL, low_X = -3^0.5, up_X = 3^0.5,
@@ -18,109 +25,6 @@ data_test <- sim_data(p = n, T = t, dim_F= q, lags_F=k, lags_X=p, ar_F=1, ar_Y=1
 
 
 data <- data_test
-
-
-Information.criteria <- function(data,n,p,q,k,t, est.method){
-
-  data_x <- data$X
-  if(est.method==1){
-    r<-(p+1)*q
-    g<-(n+t)/(n*t) *log(min(n,t))
-    kmax<-8
-    PC.IC <- function(num){
-      pca_est <- pca_estimator(X=data$X,number = num)
-      rss<-sum(diag(t(data$X-(pca_est$Lambda%*%pca_est$F*n))%*%(data$X-(pca_est$Lambda%*%pca_est$F*n)))/(n*T))
-      bayesian <- log(n)*num + n*log(rss)
-      baing <- g*num + log(rss)
-      return(data.frame(bayesian, baing))
-
-    }
-
-    output1<- sapply(1:kmax, PC.IC)
-    BIC.PC<-unlist(output1[1,])
-    BaiNg.PC<- unlist(output1[2,])
-    num.bic<- match(min(BIC.PC),BIC.PC)
-    num.BaiNg<-match(min(BaiNg.PC),BaiNg.PC)
-    delta_bic<- BIC.PC - min(BIC.PC)
-    delta_baing<- BaiNg.PC -min(BaiNg.PC)
-
-    return(data.frame("true_r"=r, "number_BIC"=num.bic, "number_BaiNg"=num.BaiNg, "bic"=delta_bic,"baing"=delta_baing))
-
-  }else if(est.method==2){
-
-    g<-(n+t)/(n*t) *log(min(n,t))
-    kmax<- 8
-    ll<-matrix(data = 0, ncol = kmax, nrow = (kmax+1))
-    
-    
-    for (p in 0:kmax) {
-
-      for(q in 1:kmax){
-
-        if((p+1)*q >n){
-          next
-        }
-       
-        est <- estimate_f(data_x=data_test,n=n,p=p,q=q,k=k,t=t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=TRUE,it=1,
-                          method = "L-BFGS-B", parallel = FALSE, max_it = 3)
-
-
-        ll[(p+1),q]<- est$value
-
-      }
-    }
-    
-    #calc_ll<- function(q,p,n){
-      
-     # if(((p+1)*q) <= n ){
-      #  est <- estimate_f(data_x=data_test,n=n,p=p,q=q,k=k,t=t,gamma_res=TRUE,lambda_res=TRUE,sigma_u_diag=TRUE,it=1,
-                          #method = "L-BFGS-B", parallel = FALSE, max_it = 3)
-        
-       # ll[(p+1),q]<- est$value
-        #return(est$value)
-        
-    #  }else{
-        
-    #  }
-    #}
-    
-    ll<- -ll
-    bayesian<-c()
-    Bai.Ng<-c()
-    bic.lag<-c()
-    Bai.Ng.lag<-c()
-
-    ML.IC <-function(num){
-      bayesian[num] <- log(n)*num - 2*log(max(ll[,num]))
-      Bai.Ng[num]<- g*num -2*log(max(ll[,num]))
-      return(data.frame(bayesian[num], Bai.Ng[num]))
-    }
-    ML.IC.Lags<- function(num_lag){
-      bic.lag[num_lag]<- log(n)*num_lag - 2*log(max(ll[num_lag,]))
-      Bai.Ng.lag[num_lag]<- g*num_lag -2*log(max(ll[,num_lag]))
-      return(data.frame(bic.lag[num_lag], Bai.Ng.lag[num_lag]))
-    }
-    output<-sapply(1:kmax, ML.IC)
-    BIC.ML<-unlist(output[1,])
-    BaiNg.ML<- unlist(output[2,])
-    num.bic<-  match(min(BIC.ML),BIC.ML)
-    num.baing<- match(min(BaiNg.ML),BaiNg.ML)
-    
-    output.lag<- sapply(1:kmax, ML.IC.Lags)
-    BIC.ML.lag<-unlist(output.lag[1,])
-    BaiNg.ML.lag<- unlist(output.lag[2,])
-    lags.bic<- match(min(BIC.ML.lag),BIC.ML.lag)
-    lags.baing<- match(min(BaiNg.ML.lag),BaiNg.ML.lag)
-
-    return(data.frame("number_BIC"=num.bic,"number_BaiNg" =num.baing, "lags_BIC"=lags.bic, "lags_BaiNg"=lags.baing))
-  }
-
-
-  else{
-    print("Choose est.method either 1 or 2")
-  }
-}
-
-Information.criteria(data = data,n=n,p=p,q=q,k=k,t=t, est.method = 2)
+#Information.criteria(data = data,n=n,p=p,q=q,k=k,t=t, est.method = 2)
 Information.criteria(data = data,n=n,p=p,q=q,k=k,t=t, est.method = 1)
 
