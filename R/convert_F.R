5073e6c005aeceaf7fea153b0e515aae8dd7175c
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-01-22 17:01:16 +0100

    Upload converting.

--- R/convert_F.R
+++ R/convert_F.R
@@ -56,42 +56,49 @@ convert_factors_ML <- function(Lambda, factors, q) {
   qp1 <- ncol(L)
   lags <- qp1/q-1
   F_ML <- factors[1:q,]
-  T <- ncol(F_ML)
 
-  help_stack <- c()
-  for (index in 1:(lags+1)) {
-    help_stack <- cbind(help_stack, factors[(qp1-q*(index)+1):(qp1-q*(index-1))])
-  }
+  if (q != 1) {
+    T <- ncol(F_ML)
 
-  F_temp <- cbind(help_stack, F_ML[,2:T])
-
-  F_stacked <- F_temp[,(lags+1):(T+lags)]
-
-  if (lags != 0) {
-    for(index in 1:(lags)){
-      F_stacked <- rbind(F_stacked,F_temp[,(lags+1-index):(T+lags-index)])
+    help_stack <- c()
+    for (index in 1:(lags+1)) {
+      help_stack <- cbind(help_stack, factors[(qp1-q*(index)+1):(qp1-q*(index-1))])
     }
-  }
-
-  V <- var(t(F_stacked))
-  theta <- eigen(V)$vectors
-  D <- diag(1/eigen(V)$values^0.5, qp1)
-  F_unc <- D %*% t(theta) %*% F_stacked
 
 
-  L_unc <- L %*% solve(D %*% t(theta))
-  QR <- qr(t(L_unc[1:qp1, 1:qp1]))
-  L_qr <- qr.Q(QR)
+    F_temp <- cbind(help_stack, F_ML[,2:T])
+
+    F_stacked <- F_temp[,(lags+1):(T+lags)]
+
+    if (lags != 0) {
+      for(index in 1:(lags)){
+        F_stacked <- rbind(F_stacked,F_temp[,(lags+1-index):(T+lags-index)])
+      }
+    }
+
+    V <- var(t(F_stacked))
+    theta <- eigen(V)$vectors
+    D <- diag(1/eigen(V)$values^0.5, qp1)
+    F_unc <- D %*% t(theta) %*% F_stacked
 
 
-  new_F <- L_qr%*%F_unc
-  new_L <- L_unc %*% L_qr
+    L_unc <- L %*% solve(D %*% t(theta))
+    QR <- qr(t(L_unc[1:qp1, 1:qp1]))
+    L_qr <- qr.Q(QR)
 
-  if (lags == 0) {
-    F_out <- new_F
+
+    new_F <- L_qr%*%F_unc
+    new_L <- L_unc %*% L_qr
+
+    if (lags == 0) {
+      F_out <- new_F
+    } else{
+      F_out <- cbind(new_F[1:q, 1:(T-lags-1)], matrix(new_F[1:qp1,T-lags], q, lags+1) )
+    }
   } else{
-    F_out <- cbind(new_F[1:q, 1:(T-lags-1)], matrix(new_F[1:qp1,T-lags], q, lags+1) )
-  }
+    F_out <- 1/var(F_ML)^0.5 * F_ML
+    new_L <- 1/var(F_ML)^0.5 * L
+    }
 
   output <- list('F' = F_out, 'Lambda' = new_L)
   return(output)
