9b17e9fd5c8de72f98b808e5bb0ed900946fb76f
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-01-27 00:22:01 +0100

    Small changes to setting.

--- simulation_files/simulation_template_try.R
+++ simulation_files/simulation_template_try.R
@@ -12,13 +12,13 @@ library(optimParallel)
 
 #-------------------Specify Iterations and other Parameters------------------------
 rm(list = ls())
-econometrician <- 'Manu' #either 'Katrin', 'Marc' or 'Manu'
+econometrician <- 'Katrin' #either 'Katrin', 'Marc' or 'Manu'
 
 q_simulation <- c(3)            #vector of number of factors per data set
-T_simulation <- seq(10, 30, 5)  #vector of number of periods per data set
-n_simulation <- c(10, 15, seq(20, 80, 20))#seq(10,60, 5)    #vector of number of signals per data set
+T_simulation <- c(7, 15, 20)  #vector of number of periods per data set
+n_simulation <- c(5, 15, 20, 30)  #seq(10,60, 5)    #vector of number of signals per data set
 
-number_iterations <- 20       #number of observations per combination of (q, T, n)
+number_iterations <- 500      #number of observations per combination of (q, T, n)
 type_sigma_U <- 'diagonal'
 
 
@@ -26,10 +26,8 @@ type_sigma_U <- 'diagonal'
 if (econometrician == 'Katrin'){
   print('Roses are red, model the shock, this code is gonna rock!')
 
-  func_vcv_mu <- function(n){#function must be named like this and return the desired vcv!
-    return(diag(n))
-  }
 
+lamb <- 1.2
 
 
 
@@ -39,23 +37,18 @@ if (econometrician == 'Katrin'){
 if (econometrician == 'Marc'){
   print('May the kernels and bug fixes be with you! #Bugsafari')
 
-  func_vcv_mu <- function(n){#function must be named like this and return the desired vcv!
-    return(diag(n))
-  }
 
 
+lamb <- 10
 
 
 
 }
 #-------------------Playground Manu----------------------------------------------
 if (econometrician == 'Manu'){
-  func_vcv_mu <- function(n){
-    return(diag(n))
-  }
 
 
-
+lamb <- 0.01
 
 
 
@@ -75,17 +68,24 @@ for (q in q_simulation) {# start for q
   for (T in T_simulation) {# start for T
     for (n in n_simulation) {# start for n
       save_iterations <- c()
+
+      Gamma_sim <- diag(-sort(-runif(q, 0.3, 0.6)))
+
+      Lambda_sim <- matrix(runif(n*q, -lamb, lamb), n, q)
+
+
       for (iterations in 1:number_iterations){ # start for iterations
         tryCatch({
         set.seed(seed_index)
         seed_index = seed_index + 1
-        vcv_mu <- func_vcv_mu(n)
+
         clusterExport(cl, list('n', 'q', 'T'))
-        data <- sim_data(p = n, T = T, dim_F= q, lags_F=1, lags_X=0, ar_F=1, ar_Y=1, low_X = -3^0.5, up_X = 3^0.5,
-                                low_F = 0.2, up_F = 0.9, burn_in = 20, data_only = FALSE, only_stationary = TRUE, vcv_mu = vcv_mu,
+        data <- sim_data(p = n, T = T, dim_F= q, lags_F=1, lags_X=0, ar_F=1, ar_Y=1, low_X = -lamb, up_X = lamb, A = list(Gamma_sim), L = list(Lambda_sim),
+                                low_F = 0.3, up_F = 0.6, burn_in = 20, data_only = FALSE, only_stationary = TRUE, vcv_mu = diag(n),
                                 adjust_diag = FALSE, geometric_F =TRUE, diag_F = TRUE, geometric_X =FALSE, geometric_Y =FALSE)
 
         X_true <- data$X
+        eigenvalues_x <- eigen(var(t(data$X)))$values
 
         #compute Information Criteria
         ic_pca <- Information.criteria(data=data,n=n,p=0,q=q,k=1,t=T, est.method=1, kmax=5, ml_parallel = TRUE, ml_maxit = 5)
@@ -242,7 +242,9 @@ for (q in q_simulation) {# start for q
         ml_trueq_mse_X <- mean((X_true - ml_trueq_f_X_hat)^2)
 
         #create vector to save over iterations
-        save_for_iterations <- c('pca_bai_right'=pca_bai_right,'pca_bic_right'=pca_bic_right,'ml_bai_right'=ml_bai_right,'ml_bic_right'=ml_bic_right,
+        ev_named <- eigenvalues_x[1:5]
+        names(ev_named) <- c('ev1', 'ev2', 'ev3','ev4', 'ev5')
+        save_for_iterations <- c(ev_named, 'pca_bai_right'=pca_bai_right,'pca_bic_right'=pca_bic_right,'ml_bai_right'=ml_bai_right,'ml_bic_right'=ml_bic_right,
                                  'bic_equal_bai' = bai_equals_bic, mses, 'VE_pca_bic' =pca_bic_f_variance_explained, 'VE_pca_bai' = pca_bai_f_variance_explained,
                                  'VE_pca_trueq' = pca_trueq_f_variance_explained, 'VE_ml_bic' =ml_bic_f_variance_explained, 'VE_ml_bai' = ml_bai_f_variance_explained,
                                  'VE_ml_trueq' = ml_trueq_f_variance_explained, 'mse_pca_bic_X_Xhat' = pca_bic_mse_X, 'mse_pca_bai_X_Xhat' = pca_bai_mse_X,
