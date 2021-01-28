8af54fea095db1a390e0649a80d213fa09e3b2e7
manuhuth <64041311+manuhuth@users.noreply.github.com>
2021-01-28 21:40:58 +0100

    Change transposed

--- plots_files/plot_pc_vs_ml_surface.R
+++ plots_files/plot_pc_vs_ml_surface.R
@@ -1,22 +1,19 @@
 #### plot difference between MSE of Maximum Likelihood and PCA
 library(tidyr)
 library(plotly)
-load("C:/Users/Mhuth/Desktop/hidiTS/simulated_data/Lambda1point2_only_pca.RData")
+load("C:/Users/Mhuth/Desktop/hidiTS/simulated_data/Lambda1point2_with_ML.RData")
 
 
-#mse_diff_f <- as.matrix(abs(simulated_data$mse_pca_f - simulated_data$mse_ml_f))
-#mse_diff_f.1<- abs(simulated_data$mse_pca_f.1 - simulated_data$mse_ml_f.1)
-#mse_diff_f.2<- abs(simulated_data$mse_pca_f.2 - simulated_data$mse_ml_f.2)
+simulated_data['mse_diff_f'] <- simulated_data$mse_pca_f - simulated_data$mse_ml_f
+simulated_data['mse_diff_f.1'] <- simulated_data$mse_pca_f.1 - simulated_data$mse_ml_f.1
+simulated_data['mse_diff_f.2'] <- simulated_data$mse_pca_f.2 - simulated_data$mse_ml_f.2
 
-#simulated_data$mse_diff_f <- mse_diff_f
-#simulated_data$mse_diff_f.1 <- mse_diff_f.1
-#simulated_data$mse_diff_f.2 <- mse_diff_f.2
 
 #n <- 150, y = 100
-maxn <- 80
-maxt <- 80
+maxn <- 140
+maxt <- 140
 
-df <- simulated_data[c('pca_bai_right','n', 'T')]
+df <- simulated_data[c('mse_diff_f','n', 'T')]
 df <- df[which( (df$T <= maxt) & (df$n <=maxn) ),]
 
 ns <- unique(df$n )
@@ -31,7 +28,7 @@ fig <- plot_ly(df_wide, x = ns, y = Ts, z = t(as.matrix(df_wide)))
 fig <- fig %>% add_surface()
 fig<- fig %>% layout(title = "", scene = list(xaxis = list(title = "n"),
   yaxis = list(title = "T"),
-  zaxis = list(title = "Bai & Ng IC")
+  zaxis = list(title = "MSE PCA - MSE ML")
 ))
 fig
 
@@ -51,7 +48,7 @@ df_wide <- reshape(df, idvar = "n", timevar = "T", direction = "wide")
 df_wide <- subset(df_wide, select=-c(n))
 #####
 
-fig <- plot_ly(df_wide, x = ns, y = Ts, z = t(as.matrix(df_wide)))
+fig <- plot_ly(df_wide, x = ns, y = Ts, z = as.matrix(df_wide))
 fig <- fig %>% add_surface(showscale = FALSE)
 fig<- fig %>% layout(title = "", scene = list(xaxis = list(title = "n"),
                                               yaxis = list(title = "T"),
